9a4748b31ce42e5789bb4c7525917bcd6d8dd3d0
Savorboard <yangxiaodong1214@126.com>
2021-05-07 23:38:58 +0800

    add layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navigation />
+    <div class="container">
+        <router-view />
+    </div>
+  </div>
+</template>
+<script>
+import Navigation from "../components/Navigation.vue";
+export default {
+  name: "Layout",
+  components: {
+    Navigation,
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-import { BootstrapVue  } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+const Foo = { template: '<div>foo</div>' }
 Vue.config.productionTip = false
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 
+const routes = [
+  { path: '/helloworld', component: Foo },
+]
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
